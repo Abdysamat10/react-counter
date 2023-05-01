b400ad9aa138784def2d84d1663283fbd03777ac
samat <samat020404@gmail.com>
2023-05-02 00:11:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [number, setNumber] = useState('');
+
+    const onCountClick = () => {
+        setCount(+number + count);
+    };
+
+    return (
+        <div className='app'>
+            <div className='container'>
+                <h1>Счетчик - <span>{count}</span></h1>
+                <p><span>Увеличить</span> счетчик</p>
+                <hr />
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input
+                        type='number'
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                    />
+                    <button
+                        className='wave-btn'
+                        onClick={onCountClick}
+                    >
+                        <span class='wave-btn__text'>Обновить</span>
+                        <span class='wave-btn__waves'></span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
